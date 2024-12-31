6afe0cd5e6c462c59366be2543a23adc4fdc2d70
Majiyeon <akwldus1216@naver.com>
2024-12-31 11:05:26 +0900

    자바 2일차 : 에제들

---
+++ java_m/src/day02/EX01_OperatorLofocal1.java
@@ -0,0 +1,21 @@
+package day02;
+
+class EX01_OperatorLofocal1 {
+
+	public static void main(String[] args) {
+		
+		
+		
+		int score = 80;
+		//gdfgdfg
+		
+		
+			
+		System.out.println();
+		
+		
+		System.out.println();
+
+	}
+
+}
